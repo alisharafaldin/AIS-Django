8f4f3d5bab1a3371326f4f4fbc97c1f32b01a6ce
HadinatAlbaraka <hadinatalbaraka@gmail.com>
2024-10-08 17:46:50 +0300

    CH_4-10-2024

--- basicinfo/urls.py
+++ basicinfo/urls.py
@@ -11,16 +11,6 @@ urlpatterns = [
     path('ajax/load_workSpecialtys/', views.load_workSpecialty, name='load_workSpecialty'),
     path('ajax/load_branchBanks/', views.load_branchBank, name='load_branchBank'),
     path('ajax/load_businessScopeSpecializations/', views.load_businessScopeSpecialization, name='load_businessScopeSpecialization')
-    # path('generalsettings', views.generalsettings, name='generalsettings'),
-    # path('countrie', views.countrie ,name='countrie'),
-    # path('edit_countrie/<countrie_id>', views.edit_countrie ,name='edit_countrie'),
-    # path('delete_countrie/<countrie_id>', views.delete_countrie ,name='delete_countrie'),
-    # path('gender', views.gender ,name='gender'),
-    # path('add_gender', views.add_gender ,name='add_gender'),
-    # path('edit/<int:gender_id>', views.edit_gender ,name='edit_gender'),
-    # path('delete/<int:gender_id>', views.delete_gender ,name='delete_gender'),  
-    # path('sponser', views.sponser ,name='sponser'),  
-    # path('add_sponser', views.add_sponser ,name='add_sponser'),
 ]
 
 
--- dalilalaemal/urls.py
+++ dalilalaemal/urls.py
@@ -4,14 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    # path('', views.dalilalaemal, name='dalilalaemal'),
     path('', views.dalil_home, name='dalil_home'),
     path('dalils', views.dalils, name='dalils'),
     path('business_Scope', views.dalils_business_Scope, name='dalils_business_Scope'),
     path('cities', views.dalil_cities, name='dalil_cities'),
     path('search', views.dalil_search, name='dalil_search'),
     path('single/<int:id>', views.dalil_single, name='dalil_single'),
-
     path('create', views.create, name='dalil_create'),
     path('update/<int:id>', views.update, name='update'),
 ]
